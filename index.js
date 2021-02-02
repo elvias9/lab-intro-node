2fcab89fed1a795f12ac786844d68039f29aa200
“Elvia <elvia.sicuro@gmail.com>
2021-02-02 17:14:51 +0100

    node intro lab done

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+        this.items.push(item);
+        this.length = this.items.length
+        this.items.sort((a,b) => a-b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    for (let item of this.items) {
+    if (this.items.indexOf(item) === pos) {
+        return item
+      } else {
+        throw new Error('OutOfBounds');
+      }
+    }
+  }
 
-  max() {}
+  max() {
+    let highest = 0
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    for (let item of this.items) {
+      if (item > highest) {
+        highest = item
+      }
+    }
+    return highest
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    let sumValues = 0
+    for (let item of this.items) {
+      sumValues += item
+    }
+    return sumValues
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    let sum = this.sum() 
+    return sum / this.items.length
+
+  }
 }
+
+let sortedList = new SortedList();
+
 
 module.exports = SortedList;
